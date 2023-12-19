7ea1dae1239fe05676c3b24d9058f2399d687e59
maksat sarsenbaev <88890025m@gmail.com>
2023-12-19 21:20:06 +0600

    added navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import '../styles/Navbar.css';
+function Navbar() {
+    return (
+        <div className="navbar">
+            <h1 className="navbar-logo">logo hi</h1>
+            <p className="navbar-item"> чччччччч</p>
+            <p className="navbar-item"> ч</p>
+            <p className="navbar-item">ы </p>
+            <p className="navbar-item">ы </p>
+        </div>
+    );
+}
+
+export default Navbar;
