aff548f05993265ddd90b4d7c803918d3b8e6048
maksat sarsenbaev <88890025m@gmail.com>
2023-12-19 22:21:16 +0600

    added image to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,18 @@
 import '../styles/Navbar.css';
+import logoSmall from '/logo-image.png';
+
 function Navbar() {
     return (
         <div className="navbar">
-            <h1 className="navbar-logo">logo hi</h1>
-            <p className="navbar-item"> чччччччч</p>
-            <p className="navbar-item"> ч</p>
-            <p className="navbar-item">ы </p>
-            <p className="navbar-item">ы </p>
+            <img className="navbar-logo-img" src={logoSmall} />
+            <h1 className="navbar-logo">HealthCare</h1>
+            <div className="navlinks-container">
+                <b className="navbar-item">Home</b>
+                <p className="navbar-item">Find a doctor</p>
+                <p className="navbar-item">Apps</p>
+                <p className="navbar-item">Testimonials</p>
+                <p className="navbar-item">About us</p>
+            </div>
         </div>
     );
 }
