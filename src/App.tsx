096c484753e1a7f7022cadea428142deb6ebf816
maksat sarsenbaev <88890025m@gmail.com>
2023-12-24 23:06:33 +0600

    added image and text with button

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,27 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 import Navbar from './components/Navbar';
+import healthImageBig1 from '/header-illustration_1.png';
 
 function App() {
     return (
         <>
             <Navbar />
-            <div className="main-container">
-                <p>text</p>
+            <div className="main-container-one">
+                <div className="main-container-one-text">
+                    <div className="main-container-one-text-inner">
+                        <h1>Virtual healthcare for you</h1>
+                        <p>
+                            Our service provides progressive, and affordable
+                            healthcare, accessible on mobile and online for
+                            everyone
+                        </p>
+                        <button className="button-primary-active">
+                            Consult today
+                        </button>
+                    </div>
+                </div>
+                <img src={healthImageBig1} />
             </div>
         </>
     );
